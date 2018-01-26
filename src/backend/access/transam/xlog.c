f2108daf39077882747bce65268c0218838c53cd
Thomas Munro <thomas.munro@enterprisedb.com>
2018-01-26 15:18:35 +1300

    Fix recovery of undo logs after a standby crash.
    A standby should never delete undo log meta data files that are referenced by
    its own control file, or it would fail to start up.
    
    RM43132, analysis and patch by Ashutosh Sharma, tweaked by me

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -6756,8 +6756,9 @@ StartupXLOG(void)
 	 * of the on-disk two-phase data.
 	 */
 	restoreTwoPhaseData();
+
 	/* Recover undo log meta data corresponding to this checkpoint. */
-	StartupUndoLogs(checkPoint.redo);
+	StartupUndoLogs(ControlFile->checkPointCopy.redo);
 
 	lastFullPageWrites = checkPoint.fullPageWrites;
 
@@ -9074,7 +9075,7 @@ CheckPointGuts(XLogRecPtr checkPointRedo, int flags)
 	CheckPointSnapBuild();
 	CheckPointLogicalRewriteHeap();
 	CheckPointBuffers(flags);	/* performs all required fsyncs */
-	CheckPointUndoLogs(checkPointRedo);
+	CheckPointUndoLogs(checkPointRedo, ControlFile->checkPointCopy.redo);
 	CheckPointReplicationOrigin();
 	/* We deliberately delay 2PC checkpointing as long as possible */
 	CheckPointTwoPhase(checkPointRedo);
@@ -9777,7 +9778,7 @@ xlog_redo(XLogReaderState *record)
 		SpinLockRelease(&XLogCtl->info_lck);
 
 		/* Write an undo log metadata snapshot. */
-		CheckPointUndoLogs(checkPoint.redo);
+		CheckPointUndoLogs(checkPoint.redo, ControlFile->checkPointCopy.redo);
 
 		/*
 		 * We should've already switched to the new TLI before replaying this
@@ -9830,7 +9831,7 @@ xlog_redo(XLogReaderState *record)
 		SpinLockRelease(&XLogCtl->info_lck);
 
 		/* Write an undo log metadata snapshot. */
-		CheckPointUndoLogs(checkPoint.redo);
+		CheckPointUndoLogs(checkPoint.redo, ControlFile->checkPointCopy.redo);
 
 		/* TLI should not change in an on-line checkpoint */
 		if (checkPoint.ThisTimeLineID != ThisTimeLineID)
