02de2483619bc8be54bf4fc5ef061f62aad13e6e
Amit Kapila <amit.kapila@enterprisedb.com>
2018-02-05 18:28:47 +0530

    Implementation of 'For Update and For Share' tuple lock modes
    This requires tuple to be locked in Exclusive or Shared mode and it
    will conflict update, delete and other modes of locks.  Currently,
    multiple lockers for shared mode are not supported, that can be done
    as a separate patch.
    
    For other types of lock modes, user will get error "unsupported lock
    mode".
    
    Amit Kapila and Kuntal Ghosh

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -1458,7 +1458,7 @@ lreplace:;
 		if (resultRelInfo->ri_NumIndices > 0)
 		{
 			if (RelationStorageIsZHeap(resultRelationDesc) &&
-				!ZHeapTupleIsInPlaceUpdated(ztuple))
+				!ZHeapTupleIsInPlaceUpdated(ztuple->t_data->t_infomask))
 			{
 				recheckIndexes = ExecInsertIndexTuples(slot, &(ztuple->t_self),
 												   estate, false, NULL, NIL);
