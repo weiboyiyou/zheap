7494f29696ef674c883b622c07424989a0aa9a7c
Kuntal Ghosh <kuntal.ghosh@enterprisedb.com>
2018-03-21 16:22:49 +0530

    Make transaction slots per zheap page as compile-time parameter
    One can specify the same using --with-trans_slots_per_page=<VALUE> while
    configuring postgres installer. Allowed values are 1,2,4,8,16,31. By default,
    it is assigned to 4 slots per page.
    The changes required for Windows have been done by Ashutosh Sharma.
    
    Patch by me, reviewed by Amit Kapila and Ashutosh Sharma

--- src/tools/msvc/Solution.pm
+++ src/tools/msvc/Solution.pm
@@ -52,6 +52,11 @@ sub _new
 	die "Bad wal_blocksize $options->{wal_blocksize}"
 	  unless grep { $_ == $options->{wal_blocksize} }
 		  (1, 2, 4, 8, 16, 32, 64);
+	$options->{trans_slots_per_page} = 4
+	  unless $options->{trans_slots_per_page};    # undef or 0 means default
+	die "Bad trans_slots_per_page $options->{trans_slots_per_page}"
+	  unless grep { $_ == $options->{trans_slots_per_page} }
+		  (1, 2, 4, 8, 16, 31);
 	$options->{wal_segsize} = 16
 	  unless $options->{wal_segsize};      # undef or 0 means default
 	die "Bad wal_segsize $options->{wal_segsize}"
@@ -180,6 +185,8 @@ s{PG_VERSION_STR "[^"]+"}{PG_VERSION_STR "PostgreSQL $self->{strver}$extraver, c
 		  1024, "\n";
 		print $o "#define XLOG_BLCKSZ ",
 		  1024 * $self->{options}->{wal_blocksize}, "\n";
+		print $o "#define ZHEAP_PAGE_TRANS_SLOTS ",
+		  $self->{options}->{trans_slots_per_page}, "\n";
 
 		if ($self->{options}->{float4byval})
 		{
